ae92981ac161c63578efba647b635ea06ff9fd88
Aga <a.kolia.grudzien@gmail.com>
2018-07-04 21:21:32 +0200

    hamburger menu fix

--- LS/app.js
+++ LS/app.js
@@ -1,1 +1,8 @@
 //hamburger menu toggle:
+ function menuToggle() {
+   const hamburgerMenu = document.querySelectorAll('.Navbar__Items--right');
+
+   hamburgerMenu.forEach(hamburger => hamburger.classList.toggle('Navbar__ToggleShow'));
+ }
+
+ document.querySelector('.Navbar__Link-toggle').addEventListener('click', menuToggle);
